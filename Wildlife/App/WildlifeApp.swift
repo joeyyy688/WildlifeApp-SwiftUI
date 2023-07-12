6352b3040d0c964bdca551e2d7ed352ea7637420
joeyyy688 <edinjoey@gmail.com>
2023-07-12 23:55:12 +0000

    Initial Commit

---
+++ Wildlife/App/WildlifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildlifeApp.swift
+//  Wildlife
+//
+//  Created by Joseph on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WildlifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
